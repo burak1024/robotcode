2096a79f0aba40c9f2bed0acd7fd94a0fa872bb8
elif-ipek-saglik <elifipeksaglik8@gmail.com>
2026-04-19 23:50:47 +0300

    Add files via upload

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public final class Constants {
+    public static final int INTAKE_MOTOR_ID = 1;
+    public static final int DEPLOY_MOTOR_ID = 2;
+    public static final double INTAKE_SPEED = 0.8;
+    public static final double OUTTAKE_SPEED = -0.8;
+    public static final double INTAKE_POSITION=10.0;
+    public static final double STOW_POSITION = 0.0;
+}
