6e367144e94a7a4cc677c330ee365f1e3d765c6a
rengoku <gamerelbistanli@gmail.com>
2026-05-21 22:21:45 +0300

    shoot on mvove

--- src/main/java/frc/robot/commands/ShootOnMove.java
+++ src/main/java/frc/robot/commands/ShootOnMove.java
@@ -2,17 +2,23 @@ package frc.robot.commands;
 
 
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import static frc.robot.Constants.HUB_POSITIONX;
+import static frc.robot.Constants.HUB_POSITIONY;
+
+import com.ctre.phoenix6.sim.ChassisReference;
+import frc.robot.Constants;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.TurretSubsystem;
-
+import edu.wpi.first.math.controller.PIDController;
 
 public class ShootOnMove extends Command{
     private final TurretSubsystem turret;
     private final CommandSwerveDrivetrain drivetrain;
-    
+    private final PIDController PID=new PIDController(0.05, 0, 0.002);
+
     public ShootOnMove(TurretSubsystem turret,CommandSwerveDrivetrain drivetrain){
     this.turret=turret;
     this.drivetrain=drivetrain;
@@ -22,27 +28,17 @@ public class ShootOnMove extends Command{
 
 @Override
 public void execute(){
+    double tx=LimelightHelpers.getTX( "limelight ");
+    boolean tv=LimelightHelpers.getTV("limelight ");
+    if(tv){
+        var ChassisSpeeds=drivetrain.getState().Speeds;
+        double chassisvy=ChassisSpeeds.vyMetersPerSecond;
+        double hpx=HUB_POSITIONX;
+        double hpy=HUB_POSITIONY;
 
-double tx=LimelightHelpers.getTX("limelight");
-boolean hastarget =LimelightHelpers.getTV("limelight");
-
-if (hastarget){
-
- double currentAngle=turret.getTurretRotation();
-
-
-var ChassisSpeeds=drivetrain.getState().Speeds;
-double robotVy=ChassisSpeeds.vyMetersPerSecond;
-
-
-
-double target=currentAngle+(tx/360.0)-(robotVy*0.05);
-turret.setTurretAngle(target);
-
-
-SmartDashboard.putNumber("Turret/MeasuredAngle", currentAngle * 360);
-}
-
+    
+    }
+        
 }
 
 
